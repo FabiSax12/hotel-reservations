b47a478c3812a3bc05e442995674068526579190
Fabián Vargas <fabidev18@gmail.com>
2026-04-17 09:54:20 -0600

    fix(i18n): remove inexistent Locale type export from index

--- packages/i18n/src/index.ts
+++ packages/i18n/src/index.ts
@@ -3,4 +3,4 @@ export { LOCALES } from "./constants/locales";
 export { createI18nContext } from "./context/createI18nContext";
 export { I18nProvider } from "./context/I18nProvider";
 export { useI18n } from "./hooks/useI18n";
-export type { Locale, TranslationMap } from "./types";
+export type { TranslationMap } from "./types";
